bac0dd3656cf615849f80c197f621867a3515fd5
Ezequias Santos <ezequiasmg16@gmail.com>
2021-08-20 19:08:04 -0300

    first

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Line, GithubLogo } from './styles';
+export const Footer: React.FC=()=>{
+ return(
+  <Container>
+   <Line/>
+   <GithubLogo/>
+  </Container>
+ );
+}
